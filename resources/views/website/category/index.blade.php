3e7eebf435dce89e4d90ca70a0b89b603bc38dba
Md. Shykat Emam <36465375+ShykatEmam@users.noreply.github.com>
2023-06-05 22:13:24 +0600

    .

--- resources/views/website/category/index.blade.php
+++ resources/views/website/category/index.blade.php
@@ -20,7 +20,7 @@
                             </a>
                             <div>
                                 <div class="post-meta"><span class="date">Culture</span> <span class="mx-1">&bullet;</span> <span>Jul 5th '22</span></div>
-                                <h3><a href="single-post.html">What is the son of Football Coach John Gruden, Deuce Gruden doing Now?</a></h3>
+                                <h3><a href="{{route('single.blog')}}">What is the son of Football Coach John Gruden, Deuce Gruden doing Now?</a></h3>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio placeat exercitationem magni voluptates dolore. Tenetur fugiat voluptates quas, nobis error deserunt aliquam temporibus sapiente, laudantium dolorum itaque libero eos deleniti?</p>
                                 <div class="d-flex align-items-center author">
                                     <div class="photo"><img src="{{asset('/')}}website/assets/img/person-2.jpg" alt="" class="img-fluid"></div>
